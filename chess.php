ce3e799e2c66cf49b17164836c71296082aa5ff0
CptJackFrost <cptjackfrost74@gmail.com>
2020-01-31 18:40:44 +0500

    all in

--- chess.php
+++ chess.php
@@ -2,9 +2,14 @@
 
 include 'class/Storage.php';
 include 'class/SQLStorage.php';
+include 'class/FileStorage.php';
+include 'class/SessionStorage.php';
 include 'class/Board.php';
 
+//$storage = new SessionStorage('map');
+//$storage = new FileStorage('save.txt');
 $storage = new SQLStorage('mysql:host=127.0.0.1;dbname=chessboard;charset=utf8', 'root', '');
+
 $board = new Board($storage);
 
 if(isset($_GET['newFigures'])){
